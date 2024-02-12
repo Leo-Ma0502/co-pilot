a910aa153dd7d10b8b428eb6ce6e361abf6b8ac5
Leo Ma <myh_00@outlook.com>
2024-02-12 06:54:13 +0000

    add env load script

---
+++ environment.sh
@@ -0,0 +1,18 @@
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt install -y python3-opencv
+sudo apt-get install -y libedgetpu1-std
+sudo apt-get install -y python3-pycoral
+sudo apt-get install -y python3-tflite-runtime
+sudo python3 -m pip install -r requirements_pi.txt
+sudo apt-get install libsdl2-mixer-2.0-0  libsdl2-2.0-0
+
+# instal ffmpeg
+cd ~ && git clone --depth 1 https://code.videolan.org/videolan/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make -j4 && sudo make install
+cd ~ && git clone git://source.ffmpeg.org/ffmpeg --depth=1
+./configure --extra-ldflags="-latomic" --arch=armel --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree
+make -j4 && sudo make install
