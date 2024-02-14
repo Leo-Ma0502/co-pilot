19fd5f3b95f9f75afe6605d591cc16fb46e49191
Leo Ma <myh_00@outlook.com>
2024-02-14 00:48:08 +0000

    update requirements

--- environment.sh
+++ environment.sh
@@ -3,13 +3,18 @@ sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key a
 sudo apt-get update
 sudo apt install -y python3-opencv
 sudo apt-get install -y libedgetpu1-std
-sudo apt-get install -y python3-pycoral
-sudo apt-get install -y python3-tflite-runtime
-sudo python3 -m pip install -r requirements_pi.txt
+# sudo apt-get install -y python3-pycoral
+# sudo apt-get install -y python3-tflite-runtime
+# sudo python3 -m
+pip install -r requirements_pi.txt
 sudo apt-get install libsdl2-mixer-2.0-0  libsdl2-2.0-0
 
+
 # instal ffmpeg
-cd ~ && git clone --depth 1 https://code.videolan.org/videolan/x264
+cd ~ 
+sudo rm -rf x264
+sudo rm -rf ffmpeg
+git clone --depth 1 https://code.videolan.org/videolan/x264
 cd x264
 ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
 make -j4 && sudo make install
