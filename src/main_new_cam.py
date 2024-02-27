0c43fde099d97d1456c716c13c90828513006407
Leo Ma <myh_00@outlook.com>
2024-02-28 12:57:07 +1300

    queuing images

--- src/main_new_cam.py
+++ src/main_new_cam.py
@@ -42,122 +42,6 @@ def recreate_fifo_file(fifo_path):
     with open(fifo_path, 'wb'):
         pass 
 
-def get_images(args):
-    # print("=== start reprocessing ===")
-    if args.cpu:
-        from tflite_runtime.interpreter import Interpreter as make_interpreter
-    else:
-        from pycoral.utils.edgetpu import make_interpreter
-
-    pubsub = PubSub()
-    camera_info = CameraInfo("config/intrinsics.yml")
-    inference_config = InferenceConfig("config/inference_config.yml")
-    image_saver = AsyncImageSaver(args.blackbox_path)
-    whitebox = WhiteBox(image_saver, args.step)
-
-    images_folder = "captured_images"
-    if not os.path.exists(images_folder):
-        os.makedirs(images_folder)
-    else:
-        shutil.rmtree(images_folder)
-        os.makedirs(images_folder)
-
-    fifo_path = 'video.h264'
-
-    recreate_fifo_file(fifo_path)
-    cmd = ['libcamera-vid', '-o', fifo_path, '--inline', '--width', '1920', '--height', '1080', '-t', '0', '--rotation', '180']
-    proc = subprocess.Popen(cmd)
-
-
-    fifo_fd = os.open(fifo_path, os.O_RDONLY | os.O_NONBLOCK)
-
-    frame_data = b''
-    count = 0
-    data_read = 0
-    start_point = 0
-
-    subprocess.run(['sudo', 'rm', '*.h264'])
-    print(f"cleared cache at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-    try:
-        copilot = CoPilot(
-            args,
-            pubsub,
-            whitebox,
-            camera_info,
-            inference_config,
-            ILed(),
-            # Speaker(args.lang),
-            make_interpreter(args.ssd_model),
-            make_interpreter(args.traffic_light_classification_model),
-        )
-    except ValueError as e:
-        print(str(e) + "Use --cpu if you do not have a coral tpu")
-        return
-
-    while True:
-        # saved = os.listdir(images_folder)
-        # if(len(saved>100)):
-        #     os.unlink(saved[-1])
-        #     print(f"cleared cache at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        try:
-            os.lseek(fifo_fd, start_point, os.SEEK_SET)
-            print(f"---\n data read: {data_read}; start: {start_point} \n---")
-
-            frame_data += os.read(fifo_fd, 1920 * 1080 * 3 * (count+1) - len(frame_data))
-        
-
-            data_read = len(frame_data)
-            temp_file_path = f"temp_frame_{count}.h264"
-            with open(temp_file_path, 'wb') as temp_file:
-                temp_file.write(frame_data[:data_read] if start_point==0 else frame_data[start_point-1:data_read])
-
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            saved_output = f"{images_folder}/image_{count}_{str(current_time)}.jpg"
-            cmd = ['ffmpeg', '-y', '-i', temp_file_path, '-f', 'image2', '-vcodec', 'mjpeg', '-vframes', '1',
-                        saved_output]
-            subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                
-
-            os.remove(temp_file_path)
-
-        
-            start_point = data_read
-
-
-            print(f"=============\n Have captured {count} images at {current_time}\n =============")
-
-            # image_queue.put(saved_output) 
-
-            if os.path.exists(saved_output):
-                print(f"-----------------\n start processing image \n -----------------")
-                file = Image.open(saved_output)
-                inference_w, inference_h = inference_config.inference_resolution
-                box = (0, 0, file.size[0], min(file.size[0] * inference_h / inference_w, file.size[1]))
-                file = file.resize(inference_config.inference_resolution, box=box)
-                copilot.process(file)
-            else:
-                 print(f"file not found")
-
-            count += 1
-        
-        except ValueError as e:
-            print(str(e))
-            stop_subprocess(proc) 
-            os.remove(temp_file_path)
-            os.remove(fifo_path)
-            return
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            stop_subprocess(proc) 
-            os.remove(temp_file_path)
-            os.remove(fifo_path)
-            break
-    copilot.stop()
-       
-# def get_lights():
-#     subprocess.run(["./reprocess.sh"])
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
@@ -250,6 +134,101 @@ def parse_arguments():
     )
     return parser.parse_args()
 
+def get_images(image_queue):
+    print("producer thread starting.......")
+
+    images_folder = "captured_images"
+    if not os.path.exists(images_folder):
+        os.makedirs(images_folder)
+    else:
+        shutil.rmtree(images_folder)
+        os.makedirs(images_folder)
+
+    fifo_path = 'video.h264'
+
+    recreate_fifo_file(fifo_path)
+    cmd = ['libcamera-vid', '-o', fifo_path, '--inline', '--width', '1920', '--height', '1080', '-t', '0', '--rotation', '180']
+    proc = subprocess.Popen(cmd)
+
+
+    fifo_fd = os.open(fifo_path, os.O_RDONLY | os.O_NONBLOCK)
+
+    frame_data = b''
+    count = 0
+    data_read = 0
+    start_point = 0
+
+    subprocess.run(['sudo', 'rm', '*.h264'])
+    print(f"cleared cache at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+    while True:
+        try:
+            os.lseek(fifo_fd, start_point, os.SEEK_SET)
+            print(f"---\n data read: {data_read}; start: {start_point} \n---")
+
+            frame_data += os.read(fifo_fd, 1920 * 1080 * 3 * (count+1) - len(frame_data))
+        
+
+            data_read = len(frame_data)
+            temp_file_path = f"temp_frame_{count}.h264"
+            with open(temp_file_path, 'wb') as temp_file:
+                temp_file.write(frame_data[:data_read] if start_point==0 else frame_data[start_point-1:data_read])
+
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            saved_output = f"{images_folder}/image_{count}_{str(current_time)}.jpg"
+            cmd = ['ffmpeg', '-y', '-i', temp_file_path, '-f', 'image2', '-vcodec', 'mjpeg', '-vframes', '1',
+                        saved_output]
+            subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                
+
+            os.remove(temp_file_path)
+
+        
+            start_point = data_read
+
+
+            print(f"=============\n Have captured {count} images at {current_time}\n =============")
+
+            image_queue.put(saved_output) 
+
+            count += 1
+        
+        except ValueError as e:
+            print(str(e))
+            stop_subprocess(proc) 
+            os.remove(temp_file_path)
+            os.remove(fifo_path)
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            stop_subprocess(proc) 
+            os.remove(temp_file_path)
+            os.remove(fifo_path)
+            break
+    image_queue.put(None)
+    # copilot.stop()
+       
+def get_lights(copilot, inference_config, image_queue):
+    print("consumer thread starting.......")
+    while True:
+        file_path = image_queue.get()
+
+        if file_path is not None:
+            print(f"-----------------\n start processing image \n -----------------")
+            if os.path.exists(file_path):
+                file = Image.open(file_path)
+                inference_w, inference_h = inference_config.inference_resolution
+                box = (0, 0, file.size[0], min(file.size[0] * inference_h / inference_w, file.size[1]))
+                file = file.resize(inference_config.inference_resolution, box=box)
+                copilot.process(file)
+            else:
+                print(f"file not found")
+        else:
+            print(f"-----------------\n no more images... \n -----------------")
+            break
+
+
+
 
 def main():
     init_sound()
@@ -260,8 +239,46 @@ def main():
     args.blackbox_path.mkdir(parents=True, exist_ok=True)
     log_path = args.blackbox_path.joinpath("co-pilot.log")
     logging.basicConfig(filename=str(log_path), level=logging.DEBUG)
-    # image_queue = queue.Queue()
-    get_images(args)
+
+
+    if args.cpu:
+        from tflite_runtime.interpreter import Interpreter as make_interpreter
+    else:
+        from pycoral.utils.edgetpu import make_interpreter
+
+    pubsub = PubSub()
+    camera_info = CameraInfo("config/intrinsics.yml")
+    inference_config = InferenceConfig("config/inference_config.yml")
+    image_saver = AsyncImageSaver(args.blackbox_path)
+    whitebox = WhiteBox(image_saver, args.step)
+
+    try:
+        copilot = CoPilot(
+            args,
+            pubsub,
+            whitebox,
+            camera_info,
+            inference_config,
+            ILed(),
+            # Speaker(args.lang),
+            make_interpreter(args.ssd_model),
+            make_interpreter(args.traffic_light_classification_model),
+        )
+    except ValueError as e:
+        print(str(e) + "Use --cpu if you do not have a coral tpu")
+        return
+
+    image_queue = queue.Queue()
+
+    thread_producer = threading.Thread(target=get_images, args=(image_queue,))
+    thread_consumer = threading.Thread(target=get_lights, args=(copilot, inference_config, image_queue))
+
+    thread_producer.start()
+    thread_consumer.start()
+
+    thread_producer.join()
+    thread_consumer.join()
+   
 
 
 if __name__ == "__main__":
